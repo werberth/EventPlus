caabedbd4a8f1873809d031018b964e89cd9df66
Werberth <werberthvinicius@gmail.com>
2017-10-29 15:47:17 -0300

    Adding static local settings

--- eventplus/settings.py
+++ eventplus/settings.py
@@ -111,13 +111,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR.child('frontend'),
 
 ]
 
-STATIC_ROOT =  BASE_DIR.child('staticfiles')
+STATIC_ROOT = BASE_DIR.child('staticfiles')
 MEDIA_ROOT = BASE_DIR.child('frontend', 'media')
 
 # Static files finding engines
--- eventplus/urls.py
+++ eventplus/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
