f2847c789d1cd5470ff766688e6e5ca71552278c
Werberth <werberthvinicius@gmail.com>
2017-10-29 15:45:59 -0300

    Add speaker model

--- eventplus/talks/models.py
+++ eventplus/talks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Speaker(models.Model):
+
+    name = models.CharField(_('Name'), max_length=255)
+    slug = models.SlugField(_('Slug'))
+    photo = models.URLField(_('Photograph'))
+    description = models.TextField(_('Description'), blank=True)
+
+    def __str__(self):
+        return self.name
