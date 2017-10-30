db7011463b93f712a85a1cd4c0604f3d1ca5251a
Werberth <werberthvinicius@gmail.com>
2017-10-30 11:58:44 -0300

    Adding  CreateEventForm

---
+++ eventplus/events/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.contrib.admin import widgets
+
+from .models import Event
+
+
+class CreateEventForm(forms.ModelForm):
+    start_date = forms.SplitDateTimeField(widget=widgets.AdminSplitDateTime())
+    end_date = forms.SplitDateTimeField(widget=widgets.AdminSplitDateTime())
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+    def clean_address(self):
+        data = self.cleaned_data['address']
+        components_of_adress = data.split(',')
+        if (len(components_of_adress) != 3):
+            raise forms.ValidationError(
+                _('Invalid Address. Make sure your address contains street, \
+                    neighborhood and house number, separated by commas.')
+            )
+        return data
