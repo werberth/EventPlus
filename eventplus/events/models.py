7e525c6baece9a95b81d915c0becbdbcc0b69a17
Werberth <werberthvinicius@gmail.com>
2017-10-29 21:38:58 -0300

    Adding Event and Supporters models

--- eventplus/events/models.py
+++ eventplus/events/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(_("Name"), max_length=155)
+    address = models.CharField(max_length=300)
+    city = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+    start_date = models.DateTimeField(_('Start Date'))
+    end_date = models.DateTimeField(_('End Date'))
+
+    def __str__(self):
+        return '{0} - {1}, {2}'.format(self.name, self.country, self.city)
+
+
+class Supporters(models.Model):
+    TYPE_CHOICES = (
+        ('sponsors', _('Sponsors')),
+        ('promoters', _('Promoters')),
+        ('organizers', _('Organizers'))
+    )
+
+    event = models.ForeignKey(
+        Event,
+        related_name='supporters',
+        verbose_name=_('Supporters')
+    )
+
+    name = models.CharField(max_length=155)
+    image_logo = models.URLField(_('Logo'))
+    types = models.CharField(
+        max_length=155,
+        choices=TYPE_CHOICES
+    )
+
+    def __str__(self):
+        return self.name
