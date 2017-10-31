6be2e2cae1a948d3ca755358808fc8cddfde374c
Werberth <werberthvinicius@gmail.com>
2017-10-30 23:37:34 -0300

    Adding Update and Delete Event views

--- eventplus/events/urls.py
+++ eventplus/events/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
 
-from .views import CreateEventView
+from .views import CreateEventView, UpdateEventView, DeleteEventView
 
 urlpatterns = [
     url(r'^create/$', CreateEventView.as_view(), name="create"),
+    url(
+        r'^update/(?P<slug>[\w-]+)$',
+        UpdateEventView.as_view(),
+        name="update"
+    ),
+    url(
+        r'^delete/(?P<slug>[\w-]+)$',
+        DeleteEventView.as_view(),
+        name="delete"
+    )
 ]
--- eventplus/events/views.py
+++ eventplus/events/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy as r
 
 from .models import Event
 from .forms import CreateEventForm
@@ -8,3 +9,17 @@ class CreateEventView(generic.CreateView):
     model = Event
     form_class = CreateEventForm
     template_name = 'events/create_event.html'
+
+
+class UpdateEventView(generic.UpdateView):
+    model = Event
+    form_class = CreateEventForm
+    template_name = 'events/create_event.html'
+
+
+class DeleteEventView(generic.DeleteView):
+    model = Event
+    success_url = r('events:create')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
