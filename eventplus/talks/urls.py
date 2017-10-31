51df8d98bff6643e02e2db2d48f614385ee07fd5
Werberth <werberthvinicius@gmail.com>
2017-10-31 01:49:22 -0300

    Adding DeleteRoomView and UpdateRoomView

--- eventplus/talks/urls.py
+++ eventplus/talks/urls.py
@@ -1,8 +1,21 @@
 from django.conf.urls import url
 
-from .views import CreateRoomView
-
+from .views import (
+    CreateRoomView,
+    UpdateRoomView,
+    DeleteRoomView
+)
 
 urlpatterns = [
     url(r'^room/create/$', CreateRoomView.as_view(), name="create"),
+    url(
+        r'^room/update/(?P<pk>\d+)/$',
+        UpdateRoomView.as_view(),
+        name="supporter_update"
+    ),
+    url(
+        r'^room/delete/(?P<pk>\d+)/$',
+        DeleteRoomView.as_view(),
+        name="supporter_delete"
+    ),
 ]
--- eventplus/talks/views.py
+++ eventplus/talks/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy as r
 
 from .models import Room
 from .forms import RoomForm
@@ -8,3 +9,19 @@ class CreateRoomView(generic.CreateView):
     model = Room
     form_class = RoomForm
     template_name = 'talks/crud_room.html'
+
+
+class UpdateRoomView(generic.UpdateView):
+    model = Room
+    form_class = RoomForm
+    template_name = 'talks/crud_room.html'
+
+
+class DeleteRoomView(generic.DeleteView):
+    model = Room
+    form_class = RoomForm
+    success_url = r('talks:create')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
