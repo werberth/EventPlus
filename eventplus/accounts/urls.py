0c4e12b2439b4ae200ce5a1424f7c03e7a149def
Werberth <werberthvinicius@gmail.com>
2017-10-31 17:57:39 -0300

    Adding routes and template to login and logout views

--- eventplus/accounts/urls.py
+++ eventplus/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 from .views import CreateUserView, UpdateUserView, ChangePasswordView
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
         r'^change-password/(?P<pk>\d+)/$',
         ChangePasswordView.as_view(),
         name='change_password'
-    )
+    ),
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
