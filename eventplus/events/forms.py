8739364adbce7a80d9a8eff5a3ca29b645cbe041
Werberth <werberthvinicius@gmail.com>
2017-11-01 00:42:46 -0300

    Adding datepicker model selector on template create_event.html

--- eventplus/events/forms.py
+++ eventplus/events/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.contrib.admin import widgets
 
 from .models import Event, Supporters
 
--- eventplus/settings.py
+++ eventplus/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compressor',
+    'widget_tweaks',
     'eventplus.accounts',
     'eventplus.talks',
     'eventplus.events'
