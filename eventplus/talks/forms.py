26187bef7c6e63234b6ac4c067481887dbc07486
Werberth <werberthvinicius@gmail.com>
2017-11-01 02:37:04 -0300

    Adding TalkForm validation

--- eventplus/talks/forms.py
+++ eventplus/talks/forms.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Room, Talk
 
@@ -13,3 +16,24 @@ class TalkForm(forms.ModelForm):
     class Meta:
         model = Talk
         fields = '__all__'
+
+    def clean(self):
+        initial = self.cleaned_data['start_at']
+        end = self.cleaned_data['end']
+        day = self.cleaned_data['date']
+        room = self.cleaned_data['room']
+
+        if(
+            Talk.objects.filter(
+                start_at__gte=initial,
+                end__lte=end,
+                date=day,
+                room=room
+            )
+        ).exists():
+            raise forms.ValidationError(
+                _("Já existe uma palestra nesse horario, verfique a lista\
+                 de palestras para saber quais horarios já extão reservados\
+                 e tente novamente")
+            )
+        return super(TalkForm, self).clean()
