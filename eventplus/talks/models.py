8aea463124bc0b2a48a4a6b58047743616fb7609
Werberth <werberthvinicius@gmail.com>
2017-11-16 21:38:01 -0300

    Made some fields like blank=True and null=True

--- eventplus/talks/models.py
+++ eventplus/talks/models.py
@@ -16,7 +16,6 @@ class Room(models.Model):
     def __str__(self):
         return "{}".format(self.name)
 
-
 class Talk(models.Model):
     event = models.ForeignKey(
         Event,
@@ -24,23 +23,25 @@ class Talk(models.Model):
         verbose_name=_('Events'),
     )
     talk_title = models.CharField(_('Talk Title'), max_length=255)
-    talk_description = models.TextField(_('Talk Description'), blank=True)
-    speaker_name = models.CharField(_('Speaker Name'), max_length=255)
-    speaker_photo = models.URLField(_('Speaker Photograph'))
+    talk_description = models.TextField(_('Talk Description'), blank=True, null=True)
+    speaker_name = models.CharField(_('Speaker Name'), max_length=255, blank=True, null=True)
+    speaker_photo = models.URLField(_('Speaker Photograph'), blank=True, null=True)
     speaker_description = models.TextField(
         _('Speaker Description'),
-        blank=True
+        blank=True,
+        null=True
     )
     room = models.ForeignKey(
         Room,
         related_name='talks',
         verbose_name=_('Room'),
-        null=False,
+        null=True,
     )
+    is_interval = models.BooleanField(_('Is Inverval'), default=False)
     start_at = models.TimeField(_('Start at'))
     end = models.TimeField(_('End'))
     date = models.DateField(_('Date'))
-    description_file = models.URLField(_('Explanatory File'))
+    description_file = models.URLField(_('Explanatory File'), blank=True, null=True)
 
     def __str__(self):
         return "{0} - {1}".format(self.talk_title, self.speaker_name)
