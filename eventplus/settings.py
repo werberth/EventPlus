9df5ad8450de7d71c6f1cc16065a3515f8c6dd6d
Werberth <werberthvinicius@gmail.com>
2017-11-18 01:53:57 -0300

    Adding i18n settings

--- eventplus/settings.py
+++ eventplus/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -150,3 +151,12 @@ STATICFILES_FINDERS = [
 COMPRESS_PRECOMPILERS = (
     ('text/x-sass', 'sassc {infile} {outfile}'),
 )
+
+# i18n settings
+USE_I18N = True
+
+LANGUAGES = (
+    ('pt-br', 'Português Brasileiro'),
+)
+
+LOCALE_PATHS = (BASE_DIR.child('eventplus', 'locale'),)
